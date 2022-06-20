2ef58a7d458f66c9dda8c59361191c44ed714b17
oyegbilepraise <oluwadhammieh@gmail.com>
2022-06-20 15:10:50 +0100

    feat: Added Loader Components

--- components/index.js
+++ components/index.js
@@ -8,3 +8,4 @@ export { default as Author } from "./Author";
 export { default as PostDetail } from "./PostDetail";
 export { default as CommentsForm } from "./CommentsForm";
 export { default as Swiper } from "./Swiper";
+export { default as Loader } from './Loader';
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { useRouter } from "next/router";
+
 import { getPosts, getPostDetails } from "../../services";
 
 import {
@@ -9,9 +11,16 @@ import {
   Author,
   Comments,
   CommentsForm,
+  Loader,
 } from "../../components";
 
 const PostDetails = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
+  
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -38,18 +47,18 @@ const PostDetails = ({ post }) => {
 export default PostDetails;
 
 export async function getStaticProps({ params }) {
-  const data = await getPostDetails(params.slug)
+  const data = await getPostDetails(params.slug);
 
   return {
     props: { post: data },
   };
 }
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
   const posts = await getPosts();
 
   return {
-    paths: posts.map(({node: {slug}}) => ({params: {slug}})),
-    fallback:false
-  }
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true,
+  };
 }
